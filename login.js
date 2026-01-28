3a912567a3cf0bfc07d2bd51a264dd47a02b2507
Prince-dev28 <princekumarjha1119@gmail.com>
2026-01-28 16:12:23 +0530

    Add initial file

---
+++ login.js
@@ -0,0 +1,30 @@
+const loginForm = document.getElementById('loginForm');
+const errorMsg = document.getElementById('errorMsg');
+
+
+const users = [
+  { username: "Prince", password: "267" },
+  { username: "Rishi", password: "296" },
+  { username: "Ritesh", password: "298" },
+ 
+];
+
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const username = document.getElementById('username').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  
+  const validUser = users.find(
+    (user) => user.username === username && user.password === password
+  );
+
+  if (validUser) {
+    
+    localStorage.setItem('loggedInUser', validUser.username);
+    window.location.href = "index.html"; 
+  } else {
+    errorMsg.textContent = "Invalid username or password!";
+  }
+});
